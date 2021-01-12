db2f04b130527368ceff108619eab3c7da9cb59a
jonathanx111 <jonathanx111@gmail.com>
2021-01-12 02:43:10 -0500

    final

--- src/components/NumberList.js
+++ src/components/NumberList.js
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { randomNumber } from "../utils";
 
-function Numbers() {
+function NumberList() {
+  const [numbers, setNumbers] = useState([1, 2, 3])
+  const [filterBy, setFilterBy] = useState("All");
+
+    let numbersToDisplay = numbers;
+    if (filterBy === "Even") {
+      numbersToDisplay = numbers.filter((num) => num % 2 === 0);
+    } else if (filterBy === "Odd") {
+      numbersToDisplay = numbers.filter((num) => num % 2 !== 0);
+    }
+  const numberList = numbersToDisplay.map((num) => <li key={num} onClick={() => handleLiClick(num)}>
+    {num}
+  </li>)
+
+
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+
   function handleAddNumber() {
     const newNumber = randomNumber();
-    console.log(newNumber);
+    const newNumberArray = [...numbers, newNumber];
+    setNumbers(newNumberArray);
+  }
+
+  function handleLiClick(numberToUpdate) {
+    const newNumberArray = numbers.map((number) => number === numberToUpdate ? number + 100 : number);
+    setNumbers(newNumberArray);
   }
 
   return (
     <div>
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="Even">Even</option>
+        <option value="Odd">Odd</option>
+      </select>
       <button onClick={handleAddNumber}>Add Number</button>
-      <ul>{/* list of numbers */}</ul>
+      <ul>{numberList}</ul>
     </div>
   );
+
 }
 
-export default Numbers;
+export default NumberList;
